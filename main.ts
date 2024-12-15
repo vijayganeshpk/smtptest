7d9534c0f8e755250bb5b2e3ae989c8c8008a9a4
Vijay Ganesh P K <1961111+vijayganeshpk@users.noreply.github.com>
2024-12-15 09:02:53 +0400

    Implement SMTP email sending functionality and update environment configuration
    Signed-off-by: Vijay Ganesh P K <1961111+vijayganeshpk@users.noreply.github.com>

--- main.ts
+++ main.ts
@@ -1,8 +1,42 @@
-export function add(a: number, b: number): number {
-  return a + b;
-}
+import nodemailer from "npm:nodemailer";
 
-// Learn more at https://docs.deno.com/runtime/manual/examples/module_metadata#concepts
-if (import.meta.main) {
-  console.log("Add 2 + 3 =", add(2, 3));
+const smtp_host = Deno.env.get("SMTP_HOST") as string;
+const smtp_port = Deno.env.get("SMTP_PORT") as string;
+const smtp_user = Deno.env.get("SMTP_USER") as string;
+const smtp_password = Deno.env.get("SMTP_PASSWORD") as string;
+const smtp_from_email = Deno.env.get("SMTP_FROM_EMAIL") as string;
+const smtp_to_email = Deno.env.get("SMTP_TO_EMAIL") as string;
+const smtp_use_tls = Deno.env.get("SMTP_USE_TLS") as string;
+
+console.log("SMTP Host:", smtp_host);
+console.log("SMTP Port:", smtp_port);
+console.log("SMTP User:", smtp_user);
+console.log("SMTP Password:", smtp_password);
+console.log("SMTP From Email:", smtp_from_email);
+console.log("SMTP To Email:", smtp_to_email);
+console.log("SMTP Use TLS:", smtp_use_tls);
+
+const transporter = nodemailer.createTransport({
+   host: smtp_host,
+   port: parseInt(smtp_port),
+   secure: smtp_use_tls.toLowerCase() === "true", // true for port 465, false for other ports
+   auth: {
+      user: smtp_user,
+      pass: smtp_password,
+   },
+});
+
+try {
+   const timestamp = new Date().toISOString();
+   const info = await transporter.sendMail({
+      from: smtp_from_email, // sender address
+      to: smtp_to_email, // list of receivers
+      subject: "Hello from smtptest (nodemailer)!", // Subject line
+      text: `Hello, this is a test email sent from smtptest(nodemailer)! Timestamp: ${timestamp}`, // plain text body
+   });
+   console.log(info);
+   console.log("Email sent successfully!");
+} catch (error) {
+   console.error(error);
+
 }
